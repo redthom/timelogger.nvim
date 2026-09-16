8092f7bc74bf751c6a619084c2629febb2c2808d
redthom <blake.smith01@gmail.com>
2026-09-16 18:08:35 -0500

    Going through :help files to find useful info.

--- lua/auto_timew/init.lua
+++ lua/auto_timew/init.lua
@@ -1,4 +1,21 @@
 local Config = require('config.lua')
 
+--[[ Notes:
+
+    vim.fs.dir({path}, {opts})                                      *vim.fs.dir()*
+      returns iterator check :help vim.fs.dir()
+
+    vim.fs.dirname({file})                                      *vim.fs.dirname()*
+      returns parent directory of {file}
+
+    events recognized in Nvim
+      check :help BufAdd, BufAdd is the beginning of the list of events.
+
+    events I think I need
+      BuffEnter
+      BuffHidden
+      BufFilePost
+
+--]]
 local M = {}
 return M
